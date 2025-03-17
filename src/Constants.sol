247e5549f182ff8a33b811890c3b0adb4c1be417
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-03-17 12:12:14 +0400

    fix: fmt

--- src/Constants.sol
+++ src/Constants.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.28;
 
 abstract contract Constants {
+    uint256 constant SCALE = 1e18;
     uint256 constant MAX_VESTING_PERIOD = 365 days;
     uint256 constant MAX_EARLY_EXIT_PENALTY_ALLOWED = 1e18;
-    uint256 constant SCALE = 1e18;
 }
